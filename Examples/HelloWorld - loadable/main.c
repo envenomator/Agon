59102e99365ec180d0c40dbf8935dc11b58a3dfd
Jeroen Venema <jeroen.venema@gmail.com>
2022-10-22 15:49:30 +0200

    Cleanup main.c comments

--- Examples/HelloWorld - loadable/main.c
+++ Examples/HelloWorld - loadable/main.c
@@ -290,41 +290,5 @@ void main(void)
 		}
 		else animationclock++;
 		
-		//while(gettimer() < 2);
-		//resettimer();
-		
-		//waitvblank();
-		//timer2 = 250;
-		//while(timer2--);
-	}
-
-	/*
-	vdp_mode(0);
-		
-	vdp_plotSetOrigin(100,100);
-	vdp_plotMoveTo(100,100);
-	vdp_plotLineTo(200,200);
-	vdp_plotColour(0,255,0);
-	vdp_plotCircleRadius(100);
-	
-	vdp_plotMoveTo(300,300);
-	vdp_plotColour(255,0,0);
-	vdp_plotCircleCircumference(350,350);
-	
-	vdp_plotColour(0,0,255);
-	vdp_plotMoveTo(400,100);
-	vdp_plotTriangle(400,100);
-	vdp_plotTriangle(400,400);
-	vdp_plotTriangle(600,400);
-	
-	vdp_plotColour(255,255,0);
-	for(y = 0; y < 480; y+=5) vdp_plotPoint(50,y);
-	getch();
-
-	vdp_fgcolour(255,255,255);
-	vdp_bgcolour(0,0,0);
-	vdp_mode(1);
-	vdp_cls();
-	*/
 	return;
 }

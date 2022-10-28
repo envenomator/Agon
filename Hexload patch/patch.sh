01d24a7d0150bd29ce1db2ada3e33e2cbad401a1
Jeroen Venema <jeroen.venema@gmail.com>
2022-10-28 19:42:01 +0200

    Typo fixed

--- Hexload patch/patch.sh
+++ Hexload patch/patch.sh
@@ -28,7 +28,7 @@ sed -i 's/#include \"agon.h\"/#include \"agon.h\"\n#include \"ihexload.h\"/g' ag
 sed -i 's/HardwareSerial DBGSerial\x280\x29;//g' agon-vdp-patched/video.ino
 sed -i 's/DBGSerial/Serial/g' agon-vdp-patched/video.ino
 sed -i 's/500000/115200/g' agon-vdp-patched/video.ino
-sed -i 's/vdu_sys_sprites.*$/vdu_sys_sprites\(\);\t\t\t\/\/ Sprite system control\n\t\t\t\tbreak;\n\t\t\tcase 0x1C:\t\t\t\t\/\/ HEXLOAD VDU 23,28\n\t\t\t\tvdu_sys_hexload\(\);/g' agon-vdp-patched/video.ino
+sed -i 's/\/\/ Sprite system control/\/\/ Sprite system control\n\t\t\t\tbreak;\n\t\t\tcase 0x1C:\t\t\t\t\/\/ HEXLOAD VDU 23,28\n\t\t\t\tvdu_sys_hexload\(\);/g' agon-vdp-patched/video.ino
 sed -i 's/Agon Quark VPD Version %d\.%02d/Agon Quark VPD Version %d\.%02d Hexload patch/g' agon-vdp-patched/video.ino
 echo "Relocating video code"
 cd agon-vdp-patched

963f7863d5f4e5d2ca1569f1bc53feb8ff07d4fb
Jeroen Venema <jeroen.venema@gmail.com>
2022-12-18 16:38:26 +0100

    VDP flashable batchfiles included with precompiled binaries

---
+++ Programs/hexload/vdp patch/batchfile-flashable-original1.02/flash.bat
@@ -0,0 +1,3 @@
+@ECHO OFF 
+ECHO Flashing to ESP32...
+".\esptool.exe" --chip esp32 --port "COM3" --baud 921600  --before default_reset --after hard_reset write_flash  -z --flash_mode dio --flash_freq 80m --flash_size 4MB 0x1000 ".\video.ino.bootloader.bin" 0x8000 ".\video.ino.partitions.bin" 0xe000 ".\boot_app0.bin" 0x10000 ".\video.ino.bin" 

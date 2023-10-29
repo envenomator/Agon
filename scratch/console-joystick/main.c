cc1ff85b4446ab2206efd054ea90229d2dbe3a85
Jeroen Venema <jeroen.venema@gmail.com>
2023-10-29 17:30:39 +0100

    Console8 joystick stuff

---
+++ scratch/console-joystick/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "mos-interface.h"
+#include "vdp.h"
+#include "agontimer.h"
+#include <ez80f92.h>
+
+int main(int argc, char * argv[]) {
+	uint8_t fire, direction;
+	PC_DDR = 0xFF; // both joysticks
+	PD_DDR |= 0xF0; // both joysticks, leave non-joystick bits 0-3 as they are
+
+	vdp_cls();
+	printf("Console8 joystick test\r\n\r\n");
+
+	while(1) {
+		fire = PD_DR;
+		direction = PC_DR;
+		vdp_cursorGoto(0, 2);
+		printf("PORT1     PORT2\r\n\r\n");
+		printf("   Up : %d    Up : %d\r\n", ((direction&0x02)?0:1), ((direction&0x01)?0:1));
+		printf(" Down : %d  Down : %d\r\n", ((direction&0x08)?0:1), ((direction&0x04)?0:1));
+		printf(" Left : %d  Left : %d\r\n", ((direction&0x20)?0:1), ((direction&0x10)?0:1));
+		printf("Right : %d Right : %d\r\n", ((direction&0x80)?0:1), ((direction&0x40)?0:1));
+		printf("BTN 1 : %d BTN 1 : %d\r\n", ((fire&0x20)?0:1), ((fire&0x10)?0:1));
+		printf("BTN 2 : %d BTN 2 : %d\r\n", ((fire&0x80)?0:1), ((fire&0x40)?0:1));
+
+		delayms(10);
+	}
+	return 0;
+}
+
